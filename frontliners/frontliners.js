fbea1c1322dc99b0e8d91154f6346c0fb25dcabc
Sandra Szenti <sandraszenti@gmail.com>
2015-02-22 10:16:25 +0000

    categories added and GA setup started

--- frontliners/frontliners.js
+++ frontliners/frontliners.js
@@ -3,11 +3,21 @@ if (Meteor.isClient) {
   Session.setDefault('rankPeople', []);
 
   Meteor.call('retrieve_doc_types', function (error, response) {
-    console.log(response);
     if (response) {
       Session.set('rankPeople', response.data);
     }
- });
+  });
+
+  Meteor.startup(function(){
+    if (Meteor.settings.public.ga) {
+      console.log('test GA');
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    }
+  })
 
   Template.body.helpers({
     rankPeople: function() {
@@ -15,6 +25,11 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.body.events({
+    "click .item": function(event){
+      $('.menu .item').tab();
+    }
+  });
 }
 
 if (Meteor.isServer) {
@@ -27,5 +42,13 @@ if (Meteor.isServer) {
            return Meteor.http.call("GET", "http://104.236.213.224:8080/scoreboard");
         }
     });
+
+    Meteor.settings = {
+      "public" : {
+        "ga": {
+          "account":"UA-60016905-1"
+        }
+      }
+    }
   });
 }
